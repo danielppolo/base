1b7a8145c1105b549a133c50db4402556aa09b27
Daniel Polo <od.polo@gmail.com>
2019-03-10 00:07:26 -0600

    Base initiative initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Typed from './components/Typed/Typed';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Typed>
+          <p>For those who gave us darkness.</p>
+          <p>30_03_19</p>
+          <p>Save the date</p>
+        </Typed>
       </div>
     );
   }
---
+++ src/components/Typed/Typed.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import style from './Typed.module.scss';
+import Typed from 'typed.js';
+
+
+const typed = props => {
+    useEffect(() => {
+        const options = {
+            stringsElement: '#typed-strings',
+            typeSpeed: 40,
+          }
+          
+        new Typed("#typed", options);
+    });
+    return <React.Fragment>
+        <div id="typed-strings">
+            {props.children}
+        </div>
+        <span id="typed"></span>
+        </React.Fragment>;
+};
+
+export default typed;
